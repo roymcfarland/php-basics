93304116c05b44ca465ed88097b62b70cca3af9c
Roy McFarland <roy.mcfarland@gmail.com>
2015-01-22 21:25:21 -0700

    Update with operators

--- index.php
+++ index.php
@@ -17,9 +17,9 @@ define('MAX_BADGES', 150000);
 <!DOCTYPE html>
 <html>
   <head>
-  	<meta charset=utf-8>
-  	<title><?php echo $name ?> | Treehouse Profile</title>
-  	<link href="css/style.css" rel="stylesheet" />
+    <meta charset=utf-8>
+    <title><?php echo $name ?> | Treehouse Profile</title>
+    <link href="css/style.css" rel="stylesheet" />
   </head>
   
   <body>
@@ -37,10 +37,8 @@ define('MAX_BADGES', 150000);
       </ul>
     </section>
     <section class="main">
-      <p>Let's Get Started!</p>
-      <p><?php echo 'Hello, World!' ?></p>
-      <pre>
-        <?php 
+      <pre><?php 
+          /*
           $names = array(
                         'roy', 
                         'laura', 
@@ -48,28 +46,20 @@ define('MAX_BADGES', 150000);
                         'ramona', 
                         'sumo'
                         );
-          print_r($names);
-        ?>
-        <?php
-        // PHP Data Types
-          // Integers
-          $one = 1;
-          $two = 2;
-          $three = 3;
-          $string_one = '1';
+          */
+          // print_r($names);
+          // $one = 1;
+          // $two = 2;
+          // $three = 3;
+          // $string_one = '1';
 
-          echo $one;
+          // echo $one;
           // echo "1";
 
-          $distance_to_home = 1.5;
-          $distance_to_work = 8.1;
-        ?>
-        <ul>
-          <li><?php echo gettype($one)?></li>
-          <li><?php echo $two + $distance_to_home + $distance_to_work?></li>
-        </ul>
-      </pre>
-      <pre><?php
+          // $distance_to_home = 1.5;
+          // $distance_to_work = 8.1;
+          // echo gettype($one)
+          // echo $two + $distance_to_home + $distance_to_work
           
           // $greeting = 'Hello, Friends!';
           // $greeting{0} = 'J';
@@ -91,7 +81,92 @@ define('MAX_BADGES', 150000);
           
           // echo YEAR;
           // echo JOB_TITLE;
-          echo MAX_BADGES;
+          // echo MAX_BADGES;
+          
+          // // ARRAYS
+          // $array_example = array();
+          // $array_example = [];
+          // $eye_colors = ['blue', 'green', 'brown'];
+          
+          // print_r($eye_colors);
+
+          // // Change value within the array
+          // $eye_colors[1] = 'hazel';
+
+          // print_r($eye_colors);
+
+          // // Add value to array
+          // $eye_colors[] = 'purple';
+
+          // print_r($eye_colors);
+  
+          // // ASSOCIATIVE ARRAYS
+          // $eye_colors = [
+            // 'roy' => 'green',
+            // 'laura' => 'brown',
+            // 'georgia' => 'unknown'
+          // ];
+
+          // print_r($eye_colors);
+
+          // $eye_colors['roy'] = 'hazel';
+
+          // echo $eye_colors['roy'];
+
+          // $eye_colors['ramona'] = 'brown';
+
+          // print_r($eye_colors);
+  
+          ////////////////////////////////
+          ////////// OPERATORS ///////////
+          ////////////////////////////////
+          
+          // // Operators
+            // $a = 10;
+            // $b = 10;
+
+            // $sum = $a + $b;
+            // $diff = $a - $b;
+            // $product = $a * $b;
+            // $quotient = $a / $b;
+
+            // echo $sum;
+            // echo $diff;
+            // echo $product;
+            // echo $quotient;
+            // echo $sum++; =21
+          
+          // // Comparison Operators
+            // $a = 10;
+            // $b = 10;
+            // $c = 20;
+            // $d = '10';
+
+            // var_dump( $a == $b ); // True
+            // var_dump( $a === $b ); // True
+            // var_dump( $a === $d ); // False
+            // var_dump( $a != $b ); // False
+            // var_dump( $a != $c ); // True
+            // var_dump( $a !== $b ); // False
+            // var_dump( $a !== $d ); // True
+            // var_dump( $a < $b ); // False
+            // var_dump( $a < $c ); // True
+            // var_dump( $a <= $b ); // True
+            // var_dump( $a >= $b ); // True
+  
+          // // Logical Operators
+            // $a = TRUE;
+            // $b = TRUE;
+            // $c = FALSE;
+
+            // var_dump( $a and $b ); // True
+            // var_dump( $a && $b ); // True
+            // var_dump( $a and $c ); // False
+            // var_dump( $a or $c ); // True
+            // var_dump( $a || $c ); // True
+            // var_dump( $b or $c ); // True
+            // var_dump( ! $a ); // False
+            // var_dump( ! $c ); // True
 
       ?></pre>
     </section>
